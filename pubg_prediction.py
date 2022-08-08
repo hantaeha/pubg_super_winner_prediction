7eab90cdb8e91c20d3afd9bf69af23d8f3201d2a
hantaeha <zkfkzkfk2017@gmail.com>
2022-08-08 16:03:01 +0900

    commit...

--- pubg_prediction.py
+++ pubg_prediction.py
@@ -1,7 +1,7 @@
+import tensorflow-probability as tfp
 import streamlit as st
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 
 hide_menu_style = """
