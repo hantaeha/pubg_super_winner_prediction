deda133cb6285d6e531b1225f8221daf681aa0bb
hantaeha <zkfkzkfk2017@gmail.com>
2022-08-10 01:20:46 +0900

    py

--- pubg_prediction.py
+++ pubg_prediction.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
-'''
+
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
-'''
+
 scaler = StandardScaler()
 
 def average_cal(kill, total):
